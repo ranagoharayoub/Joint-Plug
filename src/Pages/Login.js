976ac63d542809ae782ff553717f898e1972d863
ranagoharayoub <ranagohar1163@gmail.com>
2021-05-02 16:38:41 +0500

    Responsive Okay

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -16,37 +16,28 @@ function Login() {
             <header className="heading-">
                 <h2 className="heading1-">LOGIN/REGISTER</h2>
             </header>
-            {/* <div className="text-">
-                <p className="simple-">LOGIN</p>
-            </div> */}
             <div className="combined-">
                 <div className="formblock-">
-                    <form onSubmit={formHandle}>
+                    <form  onSubmit={formHandle}>
                     <label for="email"className="email-" required>E-mail</label><br></br>
                     <input value={email} onChange={e => setemail(e.target.value)} className="emailbox-" type="email" placeholder="Enter E-mail" id="email" name="email"></input>
                     <label for="password"className="password-">Password</label><br></br>
                     <input value={password} onChange={e => setpassword(e.target.value)} className="passwordbox-" type="password" placeholder="Enter Password" id="password" name="password"></input><br></br>
                         <button className="register-" type='submit'>Sign In</button>
                         <button className="register1-">Forgot Password</button>
-                </form>
-            </div>
+                    </form>
+                </div>
                 <div className="member-">
                     <div className="loginbox-">
                         <div className="logintext-">NEW USER</div>
                         <Link to='/join'><button className="loginbtn-">Sign Up</button></Link>
                     </div>
                     <div className="socialtext-">
-                        <p className="socialtxt-">OR</p>
-                        <p className="socialtxt-">Sign In using<br></br>social account</p>
-                        <div className="socialbtn-">
+                        <p className="or">OR</p>
+                        <p className="sign-in-">Sign In using<br></br>social account</p>
                         <button className="socialbtn1-">Login with Facebook</button>
-                        </div>
-                        <div className="socialbtn-">
                         <button className="socialbtn2-">Login with Twitter</button>
-                        </div>
-                        <div className="socialbtn-">
                         <button className="socialbtn3-">Login with Google</button>
-                        </div>
                     </div>
                 </div>
             </div>
